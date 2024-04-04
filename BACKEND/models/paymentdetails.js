1f7ea5dca6d1877c1a8909e156061ef79643c31e
IT22099518 <nuwanisithara.com@gmail.com>
2024-04-04 09:46:38 +0530

    Added paymentdetails.js

---
+++ BACKEND/models/paymentdetails.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const paymentdetailsSchema = new Schema({
+
+    paymentId : {
+        type : String,
+        required : true
+    },
+    paymentType : {
+        type : String,
+        required : true
+    },
+    amount : {
+        type : Number,
+        required : true
+    },
+    paymentDescription : {
+        type : String,
+        required : true
+    }
+})
+
+const paymentDetails = mongoose.model("paymentDetails", paymentdetailsSchema);
+
+module.exports = paymentDetails;
