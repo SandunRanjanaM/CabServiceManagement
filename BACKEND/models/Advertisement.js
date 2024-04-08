9136c236a7f009dca8e2235784f0ee48618278f8
SandunRanjanaM <sandunranjana2001@gmail.com>
2024-04-08 21:11:44 +0530

    Added Advertisement.js

---
+++ BACKEND/models/Advertisement.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const advertisementSchema = new Schema ({
+
+    title : {
+
+        type : String,
+        required : true
+    },
+
+    description : {
+
+        type : String,
+        required : true
+    },
+
+    content : {
+
+        data : Buffer,
+        contentType : [String]
+    }
+})
+
+const Advertisement = mongoose.model("Advertisement", advertisementSchema);
+
+module.exports = Advertisement;
