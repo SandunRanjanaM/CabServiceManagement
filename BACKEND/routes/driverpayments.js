da25c7dc870385f77ec5f5bd0f0afc9181a5d09a
IT22099518 <nuwanisithara.com@gmail.com>
2024-04-10 12:14:12 +0530

    create driverpayments route

---
+++ BACKEND/routes/driverpayments.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+let driverPayments = require("../models/driverpayments");
+
+//create
+router.route("/add").post((req, res) => {
+    
+})
+
+//read
+//update
+//delete
+//fetch
