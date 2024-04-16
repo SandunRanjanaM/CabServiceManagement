231c5947dff36b57b94b98064f41985fa2b50d79
IT22099518 <nuwanisithara.com@gmail.com>
2024-04-17 00:35:39 +0530

    Create Payments file

---
+++ frontend/src/components/Payments.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+
+
+function Payments() {
+
+    return (
+        <div>
+            <div class="d-grid gap-2 col-6 mx-auto">
+            <button class="btn btn-primary" type="button">Add Payment Details</button>
+            <button class="btn btn-primary" type="button">Driver's Payment Calculation</button>
+            </div>
+        </div>
+    )
+
+}
+
+export default Payments;
+
