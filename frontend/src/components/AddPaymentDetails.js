30166ab173c154b0b92a360150c5e010cb937195
IT22099518 <nuwanisithara.com@gmail.com>
2024-04-18 22:59:48 +0530

    Added new lines to the AddPaymentDetails componenet

--- frontend/src/components/AddPaymentDetails.js
+++ frontend/src/components/AddPaymentDetails.js
@@ -1,31 +1,78 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 
 export default function AddPaymentDetails() {
 
+    const [paymentType, setPaymentType] = useState("");
+    const [amount, setAmount] = useState("");
+    const [paymentDescription, setPaymentDescription] = useState("");
+
+    function sendData(e) {
+        e.preventDefault();
+        
+        const newPayment = {
+            paymentType,
+            amount,
+            paymentDescription
+        }
+
+        axios.post("http://localhost:8070/paymentdetails/add", newPayment).then(() => {
+            alert("Student Added")
+            
+            setPaymentType("");
+            setAmount("");
+            setPaymentDescription("");
+
+
+        }).catch((err) => {
+            alert(err)
+        })
+
+    }
 
     return (
 
         <div className="container">
-            <form>
+            <form onSubmit={sendData}>
 
                 <legend>Enter Payment Details</legend>
 
-            <div class="mb-3">
-                <label for="paymentType" class="form-label">Payment Type</label>
-                <input type="text" class="form-control" id="paymentType" placeholder="Enter payment type"></input>
+            <div className="mb-3">
+                <label for="paymentType" className="form-label">Payment Type</label>
+                <input type="text" className="form-control" id="paymentType" placeholder="Enter payment type" 
+                onChange={(e) => {
+
+                    setPaymentType(e.target.value);
+
+                }}>
+                </input>
             </div>
 
-            <div class="mb-3">
-                <label for="amount" class="form-label">Amount</label>
-                <input type="number" class="form-control" id="amount"></input>
+            <div className="mb-3">
+                <label for="amount" className="form-label">Amount</label>
+                <input type="number" className="form-control" id="amount" placeholder="Enter Amount" 
+                onChange={(e) => {
+
+                    setAmount(e.target.value);
+
+                }}></input>
             </div>
 
-            <div class="mb-3">
-                <label for="paymentDescription" class="form-label">Payment Description</label>
-                <input type="text" class="form-control" id="paymentDescription" placeholder="Enter payment description"></input>
+            <div className="mb-3">
+                <label for="paymentDescription" className="form-label">Payment Description</label>
+                <input type="text" className="form-control" id="paymentDescription" placeholder="Enter payment description" 
+                onChange={(e) => {
+
+                    setPaymentDescription(e.target.value);
+
+                }}></input>
             </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">Submit</button>
+
+            <Link to="/all" className="btn btn-primary">All Payment Details</Link>
             </form>
         </div>
         
