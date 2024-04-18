9ebad96253cfcd843d225663c7ba88281cb343fb
IT22099518 <nuwanisithara.com@gmail.com>
2024-04-18 22:58:35 +0530

    added new line in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import AddPaymentDetails from './components/AddPaymentDetails';
 import Header from './components/Header';
 import Payments from './components/Payments';
+import AllPaymentDetails from './components/AllPaymentDetails';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 function App() {
@@ -13,7 +14,9 @@ function App() {
         <Header/>
         
         <Routes>
-        <Route path="/payments" exact components={Payments}/>
+        <Route path="/payments" element={<Payments />} />
+        <Route path="/add" element={<AddPaymentDetails />} />
+        <Route path="/all" element={<AllPaymentDetails />} />
 
         </Routes>
       
