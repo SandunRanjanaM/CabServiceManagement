55127b79099909b1de46b8c3b2c2a292cf598283
IT22099518 <nuwanisithara.com@gmail.com>
2024-04-18 23:01:36 +0530

    Did some corrections to the payment component

--- frontend/src/components/Payments.js
+++ frontend/src/components/Payments.js
@@ -1,13 +1,14 @@
-import React, {useState} from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 
 function Payments() {
 
     return (
         <div>
-            <div class="d-grid gap-2 col-6 mx-auto">
-            <button class="btn btn-primary" type="button">Add Payment Details</button>
-            <button class="btn btn-primary" type="button">Driver's Payment Calculation</button>
+            <div className="d-grid gap-2 col-6 mx-auto">
+            <Link to="/add" className="btn btn-primary">Add Payment Details</Link>
+            <button className="btn btn-primary" type="button">Driver's Payment Calculation</button>
             </div>
         </div>
     )
