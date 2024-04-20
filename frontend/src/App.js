3ed7c6567bbfcc5caf40e49e38e594f171a12493
SandunRanjanaM <sandunranjana2001@gmail.com>
2024-04-20 10:47:19 +0530

    Modified App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import AdCreate from './components/AdCreate';
+import Header from './components/Header';
+import {BrowserRouter as  Router, Route, Routes} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
+    <Router>
+        <div className="App">
+          <Header/>
+            <Routes>
+              <Route path="/add" element={<AdCreate/>} />
+            </Routes>
+        </div>
+    </Router>
   );
 }
 
