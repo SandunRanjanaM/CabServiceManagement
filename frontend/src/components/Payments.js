80867200b3bae0395389d6f6b8e7080065772409
IT22099518 <nuwanisithara.com@gmail.com>
2024-04-22 12:30:33 +0530

    Add new lines in Payments.js

--- frontend/src/components/Payments.js
+++ frontend/src/components/Payments.js
@@ -8,7 +8,7 @@ function Payments() {
         <div>
             <div className="d-grid gap-2 col-6 mx-auto">
             <Link to="/add" className="btn btn-primary">Add Payment Details</Link>
-            <button className="btn btn-primary" type="button">Driver's Payment Calculation</button>
+            <Link to="/adddriverpayment" className="btn btn-primary">Calculate Driver Payments</Link>
             </div>
         </div>
     )
