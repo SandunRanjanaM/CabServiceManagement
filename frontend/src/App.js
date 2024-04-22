cefa214aaabd96f3f184ef10c3b5d35d60d095fd
IT22099518 <nuwanisithara.com@gmail.com>
2024-04-22 12:26:30 +0530

    added some new route paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,11 @@ import AddPaymentDetails from './components/AddPaymentDetails';
 import Header from './components/Header';
 import Payments from './components/Payments';
 import AllPaymentDetails from './components/AllPaymentDetails';
+//import UpdatePaymentDetails from './components/UpdatePaymentDetails';
+import AddReports from './components/AddReports';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import AllPaymentReports from './components/AllPaymentReport';
+import AddDriversPayments from './components/AddDriversPayments';
 
 function App() {
   return (
@@ -17,6 +21,12 @@ function App() {
         <Route path="/payments" element={<Payments />} />
         <Route path="/add" element={<AddPaymentDetails />} />
         <Route path="/all" element={<AllPaymentDetails />} />
+        <Route path="/addreports" element={<AddReports />} />
+        <Route path="/allreports" element={<AllPaymentReports />} />
+        <Route path="/adddriverpayment" element={<AddDriversPayments />} />
+
+        
+
 
         </Routes>
       
