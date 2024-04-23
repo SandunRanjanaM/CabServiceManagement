186ab64bcf0fe045253e79499b6d4e276b366852
SandunRanjanaM <sandunranjana2001@gmail.com>
2024-04-23 09:54:29 +0530

    Add css

--- frontend/src/components/AllAds.js
+++ frontend/src/components/AllAds.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import '../styles/AllAds.css'; // Adjust the import path
 
 export default function AllAds() {
     const [advertisements, setAdvertisements] = useState([]);
