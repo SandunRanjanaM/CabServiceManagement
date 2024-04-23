b58a43ce00afa8850ffac170cd47af5fd7af8c53
SandunRanjanaM <sandunranjana2001@gmail.com>
2024-04-23 12:43:06 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import AdCreate from './components/AdCreate';
 import Header from './components/Header';
 import AllAds from './components/AllAds';
+import ManageAds from './components/ManageAds';
 import UpdateAd from './components/UpdateAd';
 import DeleteAd from './components/DeleteAd';
 import {BrowserRouter as  Router, Route, Routes} from "react-router-dom"
@@ -13,6 +14,7 @@ function App() {
           <Header/>
             <Routes>
               <Route path="/add" element={<AdCreate/>} />
+              <Route path="/manage" element={<ManageAds/>} />
               <Route path="/delete/:id" element={<DeleteAd/>} />
               <Route path="/update/:id" element={<UpdateAd/>} />
               <Route path="/" element={<AllAds/>} />
