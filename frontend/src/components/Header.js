64f379aa355c0b37da618cd30cd9e15481979f80
SandunRanjanaM <sandunranjana2001@gmail.com>
2024-04-23 09:09:34 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 function Header() {
 
@@ -14,10 +15,10 @@ function Header() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    <Link to="/" className="nav-link">Home</Link>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="/add">Create Ads</a>
+                    <Link to="/add" className="nav-link">Create Ads</Link>
                     </li>
                 </ul>
                 </div>
