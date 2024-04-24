67947bf3cc281a4af435db30c8f53f7927d4f33f
SandunRanjanaM <sandunranjana2001@gmail.com>
2024-04-25 00:14:26 +0530

    Update advertisements.js

--- BACKEND/routes/advertisements.js
+++ BACKEND/routes/advertisements.js
@@ -30,14 +30,14 @@ const upload = multer({
 
 
 module.exports = function(upload) { // Accept upload middleware as parameter
-    router.route("/add").post(upload.array('content', 10), upload.single('payment'), (req, res) => { // Accept multiple images with a limit of 5
+    router.route("/add").post(upload.array('content', 10), (req, res) => { // Accept multiple images with a limit of 5
         const { title, description, email, contact } = req.body;
         const contentPaths = req.files ? req.files.map(file => file.path) : []; // Get paths of uploaded images
-        const paymentPath = req.file ? req.file.path : '';
+        const paymentPaths = req.file ? req.file.path : '';
 
         // Check if required fields are missing
     if (!title || !description || !email || !contact) {
-        return res.status(400).json({ error: "Title, description, email, and contact are required" });
+        return res.status(400).json({ error: "Title, description, email and contact are required" });
     }
 
         const newAdvertisement = new Advertisement({
