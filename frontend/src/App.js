4c39711410c47db6d685e76ceb7d8fe795c4957c
SandunRanjanaM <sandunranjana2001@gmail.com>
2024-04-24 23:02:00 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import AdminStatistic from './components/AdminStatistic';
 import AdPayment from './components/AdPayment';
 import Header from './components/Header';
 import AllAds from './components/AllAds';
+import PayApprove from './components/PayApprove';
 import ManageAds from './components/ManageAds';
 import UpdateAd from './components/UpdateAd';
 import DeleteAd from './components/DeleteAd';
@@ -25,6 +26,7 @@ function App() {
               <Route path="/update/:id" element={<UpdateAd/>} />
               <Route path="/mdelete/:id" element={<ManagerDelete/>} />
               <Route path="/mupdate/:id" element={<ManagerUpdate/>} />
+              <Route path="/get" element={<PayApprove/>} />
               <Route path="/" element={<AllAds/>} />
             </Routes>
         </div>
