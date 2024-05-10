6aedf77dc684359beecbd6b7afe0f2e7227249c3
Bhawani Adithya <165871969+Bhawi5260@users.noreply.github.com>
2024-05-10 16:04:38 +0530

    I make a seperate branch TripOrderManagement and after that committed

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,24 +1,16 @@
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const app = express();
+
 require("dotenv").config();
-const multer = require("multer");
-//import file system.
-const fs = require('fs');
-const path = require("path");
-
-app.use(bodyParser.json({limit: '10mb'}));
-app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
-app.use(express.json());
-
+const app = express();
 const PORT = process.env.PORT || 8070;
 
 app.use(cors());
 app.use(bodyParser.json());
-
 const URL = process.env.MONGODB_URL;
 
 mongoose.connect(URL, {
@@ -29,50 +21,14 @@ mongoose.connect(URL, {
     //useFindAndModify: false
 });
 
-//multer has option called disk storage.2 parameters --> destination and file name.
-//First we save the images in the computer, and then move it to MongoDB
-const storage = multer.diskStorage({
-
-    //creates a folder called uploads and stores the files in it.
-     destination:(req,file,cb)=>{
-     //cb is the callback.
-     cb(null,'uploads')
-     },
-     filename:(req,file,cb) => {
-         //since we could receive multiple files, we are going to store it with the original name.
-         cb(null,file.originalname);
-     },
- });
- 
- //Specify the storage as multer storage.
- const upload = multer({
-    //Specify the storage as our "Storage" that we created.
-    storage:storage,
- //since we are uploading files one by one, we have to make use of "single".
- //we are going to upload images using this name (testImage).
- //since we are uploading files one by one, should make use of "single"
-    limits: {
-        fileSize: 10 * 1024 * 1024, // 10 MB
-    },
- });
-
 const connection = mongoose.connection;
 
 connection.once("open", () => {
-
     console.log("Mongodb Connection Success!");
-})
-
-const advertisementRouter = require("./routes/advertisements.js")(upload); // Pass upload to advertisements router
-app.use("/advertisement", advertisementRouter);
-
-// Serve uploaded images statically
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-const CustomerRouter =require("./routes/Customers.js") 
-app.use("/Customer",CustomerRouter)
-
+});
+const tripRouter = require("./routes/trips.js");
+app.use("/trip",tripRouter);
 
 app.listen(PORT, () => {
-
     console.log(`Server is up and running on port number : ${PORT}`)
-})
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,40 @@
-import './App.css';
-import AdCreate from './components/AdCreate';
-import AdminStatistic from './components/AdminStatistic';
-import AdPayment from './components/AdPayment';
+import React, { useEffect } from 'react'; 
 import Header from './components/Header';
-import AllAds from './components/AllAds';
-import PayApprove from './components/PayApprove';
-import ManageAds from './components/ManageAds';
-import UpdateAd from './components/UpdateAd';
-import DeleteAd from './components/DeleteAd';
-import ManagerDelete from './components/ManagerDelete';
-import ManagerUpdate from './components/ManagerUpdate';
-import HomePage from './components/HomePage';
-import React from "react";
+import AddTrip from './components/AddTrip';
+import AllTrips from './components/AllTrips';
+import Payment from './components/Payment';
+import Report from './components/Report';
+import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AddCustomer from "./components/AddCustomer";
-import Home from "./components/Home";
-import UserProfile from "./components/UserProfile";
-import Update from "./components/update";
-import ManageUsers from "./components/manageusers";
-import Login from "./components/Login";
-import Navbar from "./components/Navbar";
-import Aboutus from "./components/Aboutus";
-import RealHome from "./components/realhome.js";
+import Update from './components/UpdateTrip';
 
 function App() {
+  const {loading}=useSelector((state)=>state.alerts);
+
+  useEffect(() => {
+    fetch('http://localhost:8070/trip/')
+      .then((response) => response.json())
+      .catch((error) => console.error('Error fetching trips:', error));
+  }, []);
+
   return (
     <Router>
-        <div className="App">
-          <Header/>
-            <Routes>
-              <Route path="/add" element={<AdCreate/>} />
-              <Route path="/statistic" element={<AdminStatistic/>} />
-              <Route path="/pay/:id" element={<AdPayment/>} />
-              <Route path="/manage" element={<ManageAds/>} />
-              <Route path="/delete/:id" element={<DeleteAd/>} />
-              <Route path="/update/:id" element={<UpdateAd/>} />
-              <Route path="/mdelete/:id" element={<ManagerDelete/>} />
-              <Route path="/mupdate/:id" element={<ManagerUpdate/>} />
-              <Route path="/get" element={<PayApprove/>} />
-              <Route path="/random" element={<HomePage/>} />
-              <Route path="/" element={<AllAds/>} />
-              <Route path="/addcus" element={<AddCustomer />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/getcus/:id" element={<UserProfile />} />
-              <Route path="/updatecus/:id" element={<Update />} />
-              <Route path="/manageusers" element={<ManageUsers />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/Navbar" element={<Navbar />} />
-              <Route path="/Aboutus" element={<Aboutus />} />
-              <Route path="/RealHome" element={<RealHome />} />
+      {loading ? (
+        null
+      ) : (
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<AllTrips />} />
+            <Route path="/addTrip" element={<AddTrip />} />
+            <Route path="/payment/:id" element={<Payment/>} />
+            <Route path="/report" element={<Report/>} />
+            <Route path="/update/:id" element={<Update />} />
 
           </Routes>
         </div>
-        </Router>
+      )}
+    </Router>
   );
 }
 
